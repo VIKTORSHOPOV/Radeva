9dc8a76ae41dfda3595f68f0aa05ee7009aa9137
Viktor <Viktor@DESKTOP-DE61IQH>
2022-10-08 21:05:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+
+var input = Console.ReadLine();
+var sings = ",.?!;".ToCharArray();
+var br = 0;
+
+foreach (char item in input)
+{
+    if (sings.Contains(item))
+        br++;
+}
+Console.WriteLine(br);
